256c051c2d879b9e3bcf1d65f6ce7c483ec6b9ba
Matthew Pagan <mastapegs01@gmail.com>
2020-11-24 12:48:03 -0500

    added a bit of styling to topbar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -6,13 +6,21 @@ import {
   Typography,
 } from '@material-ui/core'
 import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  }
+}))
 
 const TopBar = () => {
+  const classes = useStyles()
   return (
     <>
       <AppBar position="fixed">
         <Toolbar>
-          <IconButton edge="start" color="inherit">
+          <IconButton className={classes.icon} edge="start" color="inherit">
             <AccountCircleIcon />
           </IconButton>
           <Typography variant="h6">GitHub User Search</Typography>
