fa5aa4a130db203f479811c14647e25ea2127fb1
Matthew Pagan <mastapegs01@gmail.com>
2020-11-24 13:03:57 -0500

    fixed error in gatsby-ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,2 +1,2 @@
 exports.wrapPageElement = require('./src/gatsby-api/WrapPageElement').default
-exports.wrapRootElement = require('./src/gatsby-api/WrapRootElement')
+exports.wrapRootElement = require('./src/gatsby-api/WrapRootElement').default
