024c644944062d5d934b33513153f7a5f643aa98
Matthew Pagan <mastapegs01@gmail.com>
2020-11-24 13:04:26 -0500

    add apollo client logic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,20 @@
 import React from "react"
+import { gql, useQuery } from '@apollo/client'
+
+const TEST_QUERY = gql`
+  query UserSearch($query: String!, $type: SearchType!) {
+    search(query: $query, type: $type) {
+      userCount
+    }
+  }
+`
 
 const Home = () => {
+  const { data: userData } = useQuery(TEST_QUERY, { variables: { query: "mastapegs", type: "USER" } })
   return (
     <>
       <h1>GitHub User Search</h1>
+      <pre>{JSON.stringify(userData, null, 2)}</pre>
     </>
   )
 }
