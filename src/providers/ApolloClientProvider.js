36f48f84a5af903bac060e2dd6fd4b69c5bd1929
Matthew Pagan <mastapegs01@gmail.com>
2020-11-24 13:29:10 -0500

    auth completed

--- src/providers/ApolloClientProvider.js
+++ src/providers/ApolloClientProvider.js
@@ -1,14 +1,23 @@
 import React from 'react'
-import { ApolloProvider, ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
+import { ApolloProvider, ApolloClient, ApolloLink, InMemoryCache, HttpLink, concat } from '@apollo/client'
 import fetch from 'cross-fetch'
 
-const link = new HttpLink({
+const authLink = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    headers: {
+      authorization: `Bearer ${'865fdc881e1b8a3f655b959848c19fcc8cfad9c9'}`
+    }
+  })
+  return forward(operation)
+})
+
+const httpLink = new HttpLink({
   uri: "https://api.github.com/graphql",
   fetch,
 })
 
 const client = new ApolloClient({
-  link,
+  link: concat(authLink, httpLink),
   cache: new InMemoryCache(),
 })
 
