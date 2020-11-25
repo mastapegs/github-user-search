99700203c5a3eb25f9506043354252b70a6fc25a
Matthew Pagan <mastapegs01@gmail.com>
2020-11-25 09:30:51 -0500

    toLocaleString the number of users found

--- src/components/UserSearch.js
+++ src/components/UserSearch.js
@@ -7,7 +7,7 @@ import SingleUser from './SingleUser'
 const UserSearch = ({ data }) => {
   return (
     <>
-      <h2>Users: {data.userCount}</h2>
+      <h2>Users Found: {parseFloat(data.userCount).toLocaleString('en')}</h2>
       <Grid container spacing={3}>
         {data.edges.map(({ node: user }) => <SingleUser user={user} />)}
       </Grid>
