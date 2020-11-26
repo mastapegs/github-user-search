c2b9951673bc03569bd381cd219933c04bcb4075
Matthew Pagan <mastapegs01@gmail.com>
2020-11-25 22:38:43 -0500

    refactor UserSearch for readability

--- src/components/UserSearch.js
+++ src/components/UserSearch.js
@@ -24,39 +24,38 @@ const UserSearch = ({
   const [backButtonDisabled, setBackButtonDisabled] = useState(true)
   const [forwardButtonDisabled, setForwardButtonDisabled] = useState(true)
 
+  const renderData = () => {
+    if (!userDataFromQuery) return
+    const {
+      search: {
+        userCount,
+        edges,
+      }
+    } = userDataFromQuery
+    return (
+      <>
+        <h2>Users Found: {parseFloat(userCount).toLocaleString('en')}</h2>
+        <Grid container spacing={2}>
+          {edges
+            .filter(({ node: { login } }) => !!login)
+            .map(({ node: user, node: { login } }) => <SingleUser user={user} key={login} />)}
+        </Grid>
+        <Grid container justify='center' spacing={5}>
+          <IconButton disabled={backButtonDisabled} color='primary'>
+            <NavigateBeforeIcon className={classes.navButtons} />
+          </IconButton>
+          <IconButton disabled={forwardButtonDisabled} color='primary'>
+            <NavigateNextIcon className={classes.navButtons} />
+          </IconButton>
+        </Grid>
+      </>
+    )
+  }
+
   return (
     <>
       {userDataIsLoading && <CircularProgress />}
-      {(() => {
-        if (!userDataFromQuery) return
-        else {
-          const {
-            search: {
-              userCount,
-              edges,
-            }
-          } = userDataFromQuery
-          return (
-            <>
-              <h2>Users Found: {parseFloat(userCount).toLocaleString('en')}</h2>
-              <Grid container spacing={2}>
-                {edges
-                  .filter(({ node: { login } }) => !!login)
-                  .map(({ node: user, node: { login } }) => <SingleUser user={user} key={login} />)}
-              </Grid>
-              <Grid container justify='center' spacing={5}>
-                <IconButton disabled={backButtonDisabled} color='primary'>
-                  <NavigateBeforeIcon className={classes.navButtons} />
-                </IconButton>
-                <IconButton disabled={forwardButtonDisabled} color='primary'>
-                  <NavigateNextIcon className={classes.navButtons} />
-                </IconButton>
-              </Grid>
-            </>
-          )
-        }
-      })()}
-
+      {renderData()}
     </>
   )
 }
