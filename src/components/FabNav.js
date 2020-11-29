b2eee6cf3f391797e7620de6028f4c59344a7bc8
Matthew Pagan <mastapegs01@gmail.com>
2020-11-29 14:29:52 -0500

    factor out FabNav component

---
+++ src/components/FabNav.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from 'react'
+import {
+  Grid,
+  Fab,
+} from '@material-ui/core'
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore'
+import NavigateNextIcon from '@material-ui/icons/NavigateNext'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  navButtons: {
+    fontSize: '2em',
+  },
+  fabGrid: {
+    position: 'fixed',
+    bottom: theme.spacing(4),
+    width: '100%',
+  }
+}))
+
+const FabNav = ({ UserSearchData, getUsers, userInput, startCursor, endCursor }) => {
+
+  const classes = useStyles()
+  const [backButtonDisabled, setBackButtonDisabled] = useState(true)
+  const [forwardButtonDisabled, setForwardButtonDisabled] = useState(true)
+
+  useEffect(() => {
+    if (UserSearchData) {
+      const {
+        search: {
+          pageInfo: {
+            hasPreviousPage,
+            hasNextPage,
+          },
+        }
+      } = UserSearchData
+      setBackButtonDisabled(!hasPreviousPage)
+      setForwardButtonDisabled(!hasNextPage)
+    }
+  }, [UserSearchData])
+
+  return (
+    <Grid className={classes.fabGrid} container justify='center' spacing={5}>
+      <Grid item>
+        <Fab disabled={backButtonDisabled} color="primary" aria-label="previous-page"
+          onClick={async () => {
+            await getUsers({
+              variables: {
+                last: 10,
+                before: startCursor,
+                query: userInput,
+                type: 'USER',
+              }
+            })
+          }}>
+          <NavigateBeforeIcon className={classes.navButtons} />
+        </Fab>
+      </Grid>
+      <Grid item>
+        <Fab disabled={forwardButtonDisabled} color="primary" aria-label="previous-page"
+          onClick={async () => {
+            await getUsers({
+              variables: {
+                first: 10,
+                after: endCursor,
+                query: userInput,
+                type: 'USER',
+              }
+            })
+          }}>
+          <NavigateNextIcon className={classes.navButtons} />
+        </Fab>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default FabNav
--- src/components/RenderUserData.js
+++ src/components/RenderUserData.js
@@ -1,48 +1,21 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import {
   Grid,
-  Fab,
 } from '@material-ui/core'
 import SingleUser from './SingleUser'
-import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore'
-import NavigateNextIcon from '@material-ui/icons/NavigateNext'
 import { makeStyles } from '@material-ui/core/styles'
+import FabNav from './FabNav'
 import formatNumber from '../utils/formatNumber'
 
 const useStyles = makeStyles(theme => ({
-  navButtons: {
-    fontSize: '2em',
-  },
   usersGrid: {
     marginBottom: theme.spacing(12),
   },
-  fabGrid: {
-    position: 'fixed',
-    bottom: theme.spacing(4),
-    width: '100%',
-  }
 }))
 
 const RenderUserData = ({ UserSearchData, getUsers, userInput }) => {
 
   const classes = useStyles()
-  const [backButtonDisabled, setBackButtonDisabled] = useState(true)
-  const [forwardButtonDisabled, setForwardButtonDisabled] = useState(true)
-
-  useEffect(() => {
-    if (UserSearchData) {
-      const {
-        search: {
-          pageInfo: {
-            hasPreviousPage,
-            hasNextPage,
-          },
-        }
-      } = UserSearchData
-      setBackButtonDisabled(!hasPreviousPage)
-      setForwardButtonDisabled(!hasNextPage)
-    }
-  }, [UserSearchData])
 
   if (!UserSearchData) return <></>
 
@@ -65,38 +38,7 @@ const RenderUserData = ({ UserSearchData, getUsers, userInput }) => {
           .filter(({ node: { login } }) => !!login)
           .map(({ node: user, node: { login } }) => <SingleUser user={user} key={login} />)}
       </Grid>
-      <Grid className={classes.fabGrid} container justify='center' spacing={5}>
-        <Grid item>
-          <Fab disabled={backButtonDisabled} color="primary" aria-label="previous-page"
-            onClick={async () => {
-              await getUsers({
-                variables: {
-                  last: 10,
-                  before: startCursor,
-                  query: userInput,
-                  type: 'USER',
-                }
-              })
-            }}>
-            <NavigateBeforeIcon className={classes.navButtons} />
-          </Fab>
-        </Grid>
-        <Grid item>
-          <Fab disabled={forwardButtonDisabled} color="primary" aria-label="previous-page"
-            onClick={async () => {
-              await getUsers({
-                variables: {
-                  first: 10,
-                  after: endCursor,
-                  query: userInput,
-                  type: 'USER',
-                }
-              })
-            }}>
-            <NavigateNextIcon className={classes.navButtons} />
-          </Fab>
-        </Grid>
-      </Grid>
+      <FabNav {...{ UserSearchData, getUsers, userInput, startCursor, endCursor }} />
     </>
   )
 }
