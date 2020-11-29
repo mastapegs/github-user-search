189f43f1060a7050a197541e74ec65ddcbd0b770
Matthew Pagan <mastapegs01@gmail.com>
2020-11-29 14:37:45 -0500

    install gatsby-plugin-portal

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,12 @@
 module.exports = {
   plugins: [
     'gatsby-theme-material-ui',
+    {
+      resolve: `gatsby-plugin-portal`,
+      options: {
+        key: 'fab',
+        id: 'fab',        
+      },
+    },
   ],
 }
